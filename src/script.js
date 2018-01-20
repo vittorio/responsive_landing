97359e358c3a0a3836de666e3b56014dcc9734ea
Vittorio <sexsportandrocknroll@gmail.com>
2018-01-20 20:37:01 +0300

    v2

--- src/script.js
+++
@@ -1,6 +0,0 @@
-var clipboard = new Clipboard('.searchForm');
-
-clipboard.on('success', function(e) {
-  document.querySelector('.status').textContent = 'text copied...';
-  e.clearSelection();
-});
